ccf7fa226dfaa8607a0a940b1c680a3ce49a4157
Faiza <faizaadulwahabfaw@gmail.com>
2025-03-10 01:03:41 -0700

    menu worked on

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -17,12 +17,8 @@ const Navbar = () => {
           {isOpen ? <FiX /> : <FiMenu />}
         </button>
 
-        {/* Navigation Links */}
-        <nav
-          // className={`${
-          //   isOpen ? "block" : "hidden"
-          // } absolute md:relative top-16 left-0 w-full md:w-auto md:flex bg-[#1a1f71] md:bg-transparent text-center md:text-left`}
-        >
+        {/* Desktop Navigation */}
+        <nav className="hidden md:flex">
           <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-10 py-4 md:py-0">
             <li><a href="/" className="hover:text-gray-300">Home</a></li>
             <li><a href="#service" className="hover:text-gray-300">Service</a></li>
@@ -31,6 +27,18 @@ const Navbar = () => {
           </ul>
         </nav>
       </div>
+
+      {/* Mobile Navigation Menu */}
+      {isOpen && (
+        <div className="md:hidden absolute top-16 left-0 w-full bg-[#1a1f71] text-center">
+          <ul className="flex flex-col space-y-4 py-4">
+            <li><a href="/" className="hover:text-gray-300 text-white" onClick={() => setIsOpen(false)}>Home</a></li>
+            <li><a href="#service" className="hover:text-gray-300 text-white" onClick={() => setIsOpen(false)}>Service</a></li>
+            <li><a href="#about" className="hover:text-gray-300 text-white" onClick={() => setIsOpen(false)}>About</a></li>
+            <li><a href="#contact" className="hover:text-gray-300 text-white" onClick={() => setIsOpen(false)}>Contact</a></li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
